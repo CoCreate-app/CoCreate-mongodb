516233509c1ff0281ebed46864fdc5a315e76cbc
frankpagan <frank@cocreate.app>
2023-09-06 20:47:22 -0600

    config update

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -2,24 +2,22 @@ module.exports = {
     "organization_id": "",
     "key": "",
     "host": "",
-
-    "sources": [{
-        "array": "files",
-        "object": {
-            "_id": "60145dc49f64ba1680b86693",
-            "name": "index.html",
-            "path": "/docs/mongodb/index.html",
-            "src": "{{./docs/index.html}}",
-            "host": [
-                "cocreate.app",
-                "general.cocreate.app"
-            ],
-            "directory": "/docs/mongodb",
-            "content-type": "text/html",
-            "public": "true",
-            "website_id": "5ffbceb7f11d2d00103c4535"
+    "sources": [
+        {
+            "array": "files",
+            "object": {
+                "_id": "60145dc49f64ba1680b86693",
+                "name": "index.html",
+                "path": "/docs/mongodb/index.html",
+                "src": "{{./docs/index.html}}",
+                "host": [
+                    "*",
+                ],
+                "directory": "/docs/mongodb",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "5ffbceb7f11d2d00103c4535"
+            }
         }
-    }
     ]
-}
-
+};
